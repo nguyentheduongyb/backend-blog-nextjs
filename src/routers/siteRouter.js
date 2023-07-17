598da1f30c933cc321a6b47aa7c350c2933b11dc
nguyentheduongyb <nguyentheduongyb@gmail.com>
2023-07-17 17:57:00 +0700

    2

--- src/routers/siteRouter.js
+++ src/routers/siteRouter.js
@@ -8,7 +8,4 @@ const UserController = require('~/app/controllers/UserController');
 // router.post('/login', UserController.signin);
 // router.put('/:id', auth, UserController.update);
 // router.delete('/:id', auth, UserController.delete);
-router.get('/', (res, req) => {
-        res.send("This is the database of Nguyen The Duong")
-});
 module.exports = router;
